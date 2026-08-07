5fe2d18ef782b59d0dd66a22b9d71d2fac16ab5b
RafaGR <110624346+RaffaGR@users.noreply.github.com>
2026-08-06 21:56:55 -0300

    A2 parte 2 + exercicio do banner + texto por variavel concluido com sucess (bem minimo mas feito com sucesso)

--- loja-tp1-angular/src/app/core/header/header.ts
+++ loja-tp1-angular/src/app/core/header/header.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Banner } from "../banner/banner";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Banner],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  titulo = "Loja TP1";
+}
